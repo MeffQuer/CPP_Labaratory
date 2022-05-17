77f8a808b9d04fbbb92d276f1eb13271563027a2
MeffQuer <71191164+MeffQuer@users.noreply.github.com>
2022-05-17 22:05:36 +0300

    Add files via upload

---
+++ Lab_8/Lab_8/Lab_8.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    cout << "Начать - 1\nЗакончить - 2\nПриступаем? ";
+    int c;
+    cin >> c;
+    while (c < 1 || c > 2)
+    {
+        cout << "Неверный ввод, повторите снова. ";
+        cin >> c;
+    }
+    if (c == 2) {
+        cout << "Программа завершена.\n";
+        return 0;
+    }
+    string text, xtext;
+    cout << "Первое слово: ";
+    cin >> text;
+    cout << "Второе слово: ";
+    cin >> xtext;
+    if (text == xtext) {
+        cout << "Слова идентичны." << endl << endl;
+    } else {
+        cout << "Слова разные" << endl << endl;
+    }
+    
+    return 0;
+}
